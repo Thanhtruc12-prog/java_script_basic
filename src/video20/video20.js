e83a6c88433bf97aa509bce5045b5adaf69c54cc
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-01 22:44:14 +0700

    source code video 20 toan tu

---
+++ src/video20/video20.js
@@ -0,0 +1,40 @@
+console.log("Video 20: Toan tu");
+
+console.log("Toán tử số học");
+//  const a = 5;
+//  const b = 10;
+//  const c = 15;
+//  const d = a * b + c;
+
+
+//  console.log(">>> a + b: ", a+b);
+//  console.log(">>> d = a * b + c: ",d);
+//  console.log("lay phan du c % b: ", c%b);
+
+
+// let a = 5;
+// const b = ++a; // ++a => tăng ngay lập tức
+// // a = 5;
+// // b = a = a + 1 = 6;
+// // a = 6;
+
+
+// const b = a++; // a++ => gán b = a rồi mới a = a+1;
+// a = 5;
+// b = 5; a = a + 1 = 5 + 1 = 6;
+// console.log("a =  ", a)
+// console.log("b = : ", b)
+
+
+// // const c = a--; 
+// const c = --a; 
+// // console.log("c: ", c)
+// console.log("a =  ", a)
+// console.log("c = : ", c)
+
+console.log("Toán tử so sánh");
+
+const a = 5; // number
+const b = "5"; // string
+
+console.log(a === b);
