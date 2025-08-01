e85f625201f0f7db4709fbc51915fbb4dd52cce0
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-01 22:04:24 +0700

    source code video 18

---
+++ src/video18/video18.js
@@ -0,0 +1,28 @@
+console.log(`Video 18: kieu du lieu`);
+
+// Number
+
+const myAge = 25;
+const myScore = 9.9;
+
+console.log("My age: ", myAge);
+console.log("My score: ", myScore);
+
+// String
+const name0 = `Thanh_Truc`;
+const name1 = "ThanhTruc";
+console.log(name0);
+console.log(name1);
+
+// boolean: true/false
+
+const isTuiDepGai = true;
+console.log(isTuiDepGai);
+
+// undefine: chua gan gia tri
+let Bla;
+console.log(Bla);
+
+// null
+let testNull = null;
+console.log(testNull);
