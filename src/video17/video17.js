5cfbdb1d3344bf81a7954fa1f6126df8ee44f47b
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-01 21:26:10 +0700

    source code khai bao bien

---
+++ src/video17/video17.js
@@ -0,0 +1,20 @@
+console.log(`video 17: Khai bao bien`);
+
+
+var name = "Alice";
+
+// Nên dùng let và const
+
+const name1 = "Thanh_Truc";
+
+// Kiểm soát phạm vi hoạt động(sử dụng) của biến số 
+let age = 20;
+
+
+// Dùng const khi khai báo giá trị không cần thay đổi (gán giá trị 1 lần)
+const MyAge = 25;
+
+
+console.log("MyAge: ",MyAge);
+
+
