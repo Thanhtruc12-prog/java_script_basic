2f71a8b8b3ba51242bc89ada8927552ac412917b
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-01 22:17:42 +0700

    source code video19

---
+++ src/video19/video19.js
@@ -0,0 +1,13 @@
+console.log(`Video 19: Kieu du lieu tham chieu`);
+
+// object: key_value,
+const person = {
+    myName: "Thanh_Truc",
+    myAge: "20",
+    myAddress: "AnGiang"
+}
+console.log(person);
+
+// array
+const score = [10, 9, 9.1, 9.9, 9.4, 9.5];
+console.log(score);
