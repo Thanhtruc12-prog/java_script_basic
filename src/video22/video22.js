675e9f1340e27b2f0f7045fbc0496f472ebe3ba3
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-02 21:16:45 +0700

    video22

---
+++ src/video22/video22.js
@@ -0,0 +1,10 @@
+ console.log("Video 22: template string");
+/*
+Template String (còn gọi là Template Literals) là cách khai báo chuỗi
+ bằng dấu backtick (`) thay vì nháy đơn ' ' hay nháy kép " ". */
+
+ const name1 = "thanh truc";
+ const age1 = 20;
+
+ const intro = `My name is ${name1}. I am ${age1} years old.`
+ console.log(intro);
