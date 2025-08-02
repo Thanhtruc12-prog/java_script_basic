acafb125fc5c099af40c6d2df4733651f7e2de19
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-02 20:58:45 +0700

    source code ve cau lenh console.log

---
+++ src/video21/video21.js
@@ -0,0 +1,17 @@
+console.log("Video 21: Câu lệnh console.log");
+
+
+console.log("information");
+console.error("oops my mistake");
+console.warn("test warning");
+// warning nhe hon error
+
+// Nên: dùng dấu phẩy (tự động thêm dấu cách) 
+let name1 = "ThanhTruc";
+let age1 = 20;
+console.log("Name:", name1, "Age:", age1); 
+
+
+
+// Tô màu cho console.log() bằng %c 
+console.log("%cCảnh báo!", "color: red; font-weight: bold;"); 
