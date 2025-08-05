cdbb316c56f728b5686113556cbf00cbba149089
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-05 21:40:38 +0700

    source code pham vi bien (scope)

---
+++ src/chapter04/video33/video33.js
@@ -0,0 +1,36 @@
+console.log("Video 33: Phạm vi biến (Scope)");
+
+/* Có 3 phạm vi (Scope)
++ Toàn cục (Global)
++ Hàm (Function)
++ Khối (Block)*/
+
+// Phạm vi toàn cục – Global Scope
+let globalVar = "Tôi là biến toàn cục"; 
+
+function show() { 
+ console.log(globalVar); // Truy cập được 
+}
+
+show(); 
+console.log(globalVar); // Truy cập được 
+
+
+// Phạm vi hàm – Function Scope
+function sayHi() { 
+    let name1 = "hoidanit"; 
+    console.log("Hi " + name1); 
+
+    // Phạm vi khối – Block Scope (ES6 trở lên)
+    if (true) { 
+    let x = 10; 
+    const y = 20; 
+    console.log(x, y); // ✅ Truy cập được 
+    } 
+    console.log(x); // ❌ Lỗi: x is not defined
+} 
+
+sayHi(); 
+console.log(name1); // ❌ Lỗi: name1 is not defined 
+
+
