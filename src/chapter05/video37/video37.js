00c6b71395666652eec6d40cd4953c1eae97b87e
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-09 21:44:36 +0700

    source code video ve mang (Array)

---
+++ src/chapter05/video37/video37.js
@@ -0,0 +1,9 @@
+console.log("Video 37: Mảng");
+
+// array
+const names = ["Thanh Truc", "HK5", "Cố lên"];
+
+const others = ["Thanh Truc", true, "20", [1, 2, 3]];
+
+console.log(names);
+console.log(others);
