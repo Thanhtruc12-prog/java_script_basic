a9dbea2c6353f8ec0090dc502571a93cf774a327
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-14 12:05:28 +0700

    source code gioi thieu object

---
+++ src/chapter05/video42/video42.js
@@ -0,0 +1,29 @@
+console.log("Video 42: Object");
+
+const scores = [10, 3, 4, 8, 6]
+
+// key: value
+const sv1 = {
+    score: 10,
+    name: "Thanh Truc",
+    address: {
+        city: "LongXuyen",
+        country: "VietNam"
+    }
+}
+
+const sv2 = {
+    score: 9,
+    name: "Tosker",
+    address: {
+        city: "LongXuyen",
+        country: "VietNam"
+    }
+}
+
+
+const sinhvien = [sv1, sv2];
+
+console.log(">>> scores: ", scores);
+console.log(">>> sinh vien: ", sinhvien);
+
