af048e73c612d10a7f6e46abd1ff1a291fd83d83
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-16 10:56:20 +0700

    source code truy cap phan tu HTML voi DOM

---
+++ src/chapter06/video49/video49.js
@@ -0,0 +1,9 @@
+console.log("Video 49: Truy cập phần tử HTML với DOM");
+
+const element = document.getElementById("username");
+
+const myDiv = document.querySelector(".hoidanit");
+
+const allMyDiv = document.querySelectorAll(".hoidanit");
+
+console.log(allMyDiv);
