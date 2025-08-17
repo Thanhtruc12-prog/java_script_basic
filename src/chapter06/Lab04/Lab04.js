e3a9502e80a88667761b6d3a75a082fadfe55707
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-17 21:31:55 +0700

    source code bai tap Lab 04

---
+++ src/chapter06/Lab04/Lab04.js
@@ -0,0 +1,37 @@
+console.log("Bài tập: Lab 04");
+
+const usernameElement = document.getElementById("username");
+
+const passwordElement = document.getElementById("password")
+
+const btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener("click", () => {
+    const userName = usernameElement.value;
+    const passWord = passwordElement.value;
+    if(userName === "hoidanit@gmail.com" && passWord ==="123456"){
+        alert("Đăng nhập thành công!");
+        window.location.href = "success.html"
+    }
+    else {
+        alert("Tài khoản hoặc mật khẩu sai");
+        usernameElement.style.borderColor = "red";
+        passwordElement.style.borderColor = "red";
+
+    }
+    console.log(usernameElement.value, passwordElement.value)
+    
+})
+
+
+// function controlBtn() {
+//     if (inputName.value == "hoidanit@gmail.com" && inputPass == 123456){
+//         alert("Đăng nhập thành công!");
+
+//     }
+//     else{
+//         alert("Tài khoản hoặc mật khẩu sai");
+//         inputName.style.borderColor = "red";
+//         inputPass.style.borderColor = "red";
+//     }
+// }
