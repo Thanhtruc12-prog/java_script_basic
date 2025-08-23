c60b53df4108a597a3932200e2909a8824d8c463
Thanh Truc <thanhtrucvotrinh@gmail.com>
2025-08-23 22:23:36 +0700

    source code async/ await

---
+++ src/chapter07/video66/video66.js
@@ -0,0 +1,17 @@
+console.log("Video 66: Async/ Await");
+
+// promise 
+fetch("http://localhost:8000/users")
+    .then(res => res.json())
+    // convert data dang response sang object
+    .then(data => console.log("fetch with promise: ",data))
+
+
+// async/ await
+const fetchData = async() => {
+    const res = await fetch("http://localhost:8000/users");
+    const data = await res.json();
+    console.log("fetch with async await: ", data);
+
+}
+fetchData();
